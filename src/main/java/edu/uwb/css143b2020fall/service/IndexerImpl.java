19283ab2efde7f73aad19ecab87d89d43258a372
Eimi <eeimikoga@gmail.com>
2020-12-10 19:47:49 -0800

    initial commit

--- src/main/java/edu/uwb/css143b2020fall/service/IndexerImpl.java
+++ src/main/java/edu/uwb/css143b2020fall/service/IndexerImpl.java
@@ -11,7 +11,18 @@ import java.util.Map;
 public class IndexerImpl implements Indexer {
     public Map<String, List<List<Integer>>> index(List<String> docs) {
         Map<String, List<List<Integer>>> indexes = new HashMap<>();
-        // add your code
+
+        //my code starts here:
+        List<List<Integer>> docNum = new ArrayList<>();
+
+        for(int i = 1; i < docs.size(); i++){
+            for(int b = 0; b < docs.get(i).length(); b++){
+                docNum.get(i).add(b);
+            }
+            for(int c = 0; c < docNum.get(i).size(); c++){
+                indexes.put(docs.get(c), docNum);
+            }
+        }
         return indexes;
     }
 }
