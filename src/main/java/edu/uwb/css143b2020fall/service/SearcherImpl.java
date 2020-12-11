01084cb3c79906437c01f54aa622098d20b4d14c
Eimi <eeimikoga@gmail.com>
2020-12-10 20:24:12 -0800

    commit 2: added to searcher, doesn't work yet.

--- src/main/java/edu/uwb/css143b2020fall/service/SearcherImpl.java
+++ src/main/java/edu/uwb/css143b2020fall/service/SearcherImpl.java
@@ -11,6 +11,14 @@ public class SearcherImpl implements Searcher {
     public List<Integer> search(String keyPhrase, Map<String, List<List<Integer>>> index) {
         List<Integer> result = new ArrayList<>();
         // add your code
+        for(int i = 0; i < index.size(); i++){
+            for(int j = 0; j < index.get(i).size(); j++){
+                String current = index.get(i).toString();
+                if(keyPhrase == current){
+                    result.add(j);
+                }
+            }
+        }
         return result;
     }
 }
